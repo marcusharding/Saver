0e702b4c348543b110de1ed4024517cad8420542
marcusharding <marcus.harding@kaldorgroup.com>
2021-11-30 20:00:47 +0000

    Add in splashscreen + app icons

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+// React
+import React from 'react';
+import {AppRegistry, SafeAreaView} from 'react-native';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// Screens
+import App from './App';
+
+const Index = () => {
+  return (
+    <SafeAreaView>
+      <App />
+    </SafeAreaView>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Index);
